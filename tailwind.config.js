1132afa8973edc2ecadb38b9e1b005b5ae3b2f99
gabriel da silva <gabrielsilvafernandes1606@gmail.com>
2023-10-18 16:27:26 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      height:{
+        0.5:'2px'
+      }
+    },borderWidth:{
+      1:'1px',
+      2:'2px'
+    },
+
+  },
+  plugins: [],
+}
+
